cdafac721cdc56fc554eec445e60bbc7614f8a14
Shubham Gupta <sguptadrums@gmail.com>
2020-08-20 16:12:40 -0700

    Installing Dependencies

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Grid } from 'semantic-ui-react';
+import { handleInitialData } from '../actions/shared';
+import { connect } from 'react-redux';
+
+class App extends Component {
+  componentDidMount() { // <- new
+    this.props.handleInitialData();
+  } // <- new
+  render() {
+    return (
+        <Router>
+          <div className="App">
+            <ContentGrid>
+              <p>New Start...</p>
+            </ContentGrid>
+          </div>
+        </Router>
+    );
+  }
+}
+
+const ContentGrid = ({ children }) => (
+    <Grid padded="vertically" columns={1} centered>
+      <Grid.Row>
+        <Grid.Column style={{ maxWidth: 550 }}>{children}</Grid.Column>
+      </Grid.Row>
+    </Grid>
+);
+
+export default connect( // <- new
+    null, // <- new
+    { handleInitialData } // <- new
+)(App); // <- new
