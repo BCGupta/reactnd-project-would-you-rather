0eb01702999dce6d2b2636eaa082408e71397700
Shubham Gupta <sguptadrums@gmail.com>
2020-08-24 15:39:10 -0700

    npm update

--- src/index.js
+++ src/index.js
@@ -7,12 +7,11 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
 import middleware from './middleware';
 
-
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, middleware);
 
 ReactDOM.render(
-<Provider store={store}>
-    <App />
+    <Provider store={store}>
+        <App />
     </Provider>,
-document.getElementById('root')
+    document.getElementById('root')
 );
